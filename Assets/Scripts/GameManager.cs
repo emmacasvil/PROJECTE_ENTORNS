afa0c9c8cf3435802c75a8de97b7e870ac380c1e
emmacasvil <emma.casassas@estudiantat.upc.edu>
2025-12-10 09:26:00 +0100

    aa

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    // Definició dels estats com a constants 
+    public const int ESTAT_DISTOPIC = 0;
+    public const int ESTAT_NORMAL = 1;
+    public const int ESTAT_UTOPIC = 2;
+
+    // Estat actual del joc
+    public int estatActual = ESTAT_DISTOPIC;
+
+    // Funció per canviar d'estat
+    public void CanviarEstat(int nouEstat)
     {
-        
+        estatActual = nouEstat;
+        Debug.Log("Nou estat: " + estatActual);
     }
+}
+
+void Update()
+{
+    if (GameManager.estatActual == GameManager.ESTAT_DISTOPIC)
+    {
+        return ESTAT_DISTOPIC;
+    }
+    else if (GameManager.estatActual == GameManager.ESTAT_NORMAL)
+    {
+        return ESTAT_NORMAL;
+    }
+    else if (GameManager.estatActual == GameManager.ESTAT_UTOPIC)
+    {
+        return ESTAT_UTOPIC;
+    }
+}
+
 }
