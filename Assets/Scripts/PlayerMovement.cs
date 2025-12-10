391ab0c571b3909682d7b6f48de695ef84242604
Xeniaa007 <xenia.jordan@estudiantat.upc.edu>
2025-12-10 09:45:14 +0100

    eliminar input handler

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    PlayerMovement _movement;
+    public float speed = 4f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+
+}
+
+
+
