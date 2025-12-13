3ea88a86879e0d5fe9307931dd2283ced6740561
emmacasvil <emma.casassas@estudiantat.upc.edu>
2025-12-13 12:04:18 +0100

    a

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,13 @@ using System;
 public class GameManager : MonoBehaviour
 {
 
-    //static vol dir que només hi ha una còpia de l'objecte. Instance és una referència global al GameManager. 
+    //static vol dir que nomes hi ha una copia de l'objecte. Instance es una referencia global al GameManager. 
     public static GameManager Instance;
 
     //Crea un event; una llista de funcions que s'executen quan hi ha un canvi. 
     public event Action<int> Canvi; 
 
-    //Declaració dels estats
+    //Declaracio dels estats
     public const int ESTAT_DISTOPIC = 0;
     public const int ESTAT_NORMAL = 1;
     public const int ESTAT_UTOPIC = 2;
@@ -18,16 +18,16 @@ public class GameManager : MonoBehaviour
     // Estat actual del joc, es va actualitzant durant la partida. 
     public int estatActual = ESTAT_DISTOPIC;
 
-    //Aquí s'indica que el Game Manager serà la instància global del joc. S'executa quan es crea l'objecte abans de l'start. 
+    //Aqui s'indica que el Game Manager sera la instancia global del joc. S'executa quan es crea l'objecte abans de l'start. 
     private void Awake()
     {
         Instance = this; 
     }
 
-    //Funció per canviar d'estat. 
-    public void CanviarEstat(int nouEstat) //Es passa el valor del nou estat per paràmetre. 
+    //Funcio per canviar d'estat. 
+    public void CanviarEstat(int nouEstat) //Es passa el valor del nou estat per parametre. 
     {
-        if(nouEstat < 0 || nouEstat > 2) //Aquí es comprova si l'estat és un dels estats establerts i en cas contrari surt de la funció. 
+        if(nouEstat < 0 || nouEstat > 2) //Aqui es comprova si l'estat es un dels estats establerts i en cas contrari surt de la funcio. 
         {
             return; 
         }
