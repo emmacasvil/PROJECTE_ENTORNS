62f667667fed839e400bcc21b7d82fb164b7d14e
emmacasvil <emma.casassas@estudiantat.upc.edu>
2025-12-14 11:22:21 +0100

    Update FlowerState.cs

--- Assets/Scripts/Flors/FlowerState.cs
+++ Assets/Scripts/Flors/FlowerState.cs
@@ -1,16 +1,71 @@
 using UnityEngine;
+using System;
 
 public class FlowerState : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Event: avisa quan canvia l’estat de la flor
+    public event Action<FlowerState, int> CanviFlor;
+
+    // Estats
+    public const int sana = 0;
+    public const int seca = 1;
+    public const int morta = 2;
+
+    public int estatActual = sana;
+
+    // Temps abans de canviar d’estat
+    [SerializeField] private float tempsSana = 10f;
+    [SerializeField] private float tempsSeca = 5f;
+
+    private Coroutine rutinaTemps;
+
     void Start()
     {
-        
+        // Quan neix, comença el compte enrere
+        rutinaTemps = StartCoroutine(ControlTemps());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ControlTemps()
     {
-        
+        yield return new WaitForSeconds(tempsSana);
+        CanviarEstat(seca);
+
+        yield return new WaitForSeconds(tempsSeca);
+        CanviarEstat(morta);
+    }
+
+    public void CanviarEstat(int nouEstat)
+    {
+        if (nouEstat < 0 || nouEstat > 2) return;
+
+        estatActual = nouEstat;
+
+        // Avisar als listeners
+        CanviFlor?.Invoke(this, estatActual);
+
+        Debug.Log("Flor canviant a estat: " + estatActual);
+
+        if (estatActual == morta)
+        {
+            DestroyFlower();
+        }
+    }
+
+    // Acció del jugador (regar, podar, etc.)
+    public void AtendreFlor()
+    {
+        if (estatActual == morta) return;
+
+        // Reiniciem el temporitzador
+        if (rutinaTemps != null)
+            StopCoroutine(rutinaTemps);
+
+        CanviarEstat(sana);
+        rutinaTemps = StartCoroutine(ControlTemps());
+    }
+
+    private void DestroyFlower()
+    {
+        Destroy(gameObject, 3f);
     }
 }
