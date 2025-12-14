cbd1ff68c9363e747b1839f889ba4788d0a23bfb
emmacasvil <emma.casassas@estudiantat.upc.edu>
2025-12-14 11:10:40 +0100

    g

---
+++ Assets/Scripts/Flors/FlowerGenerator.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System;
+
+///AQUEST CODI ES UN EXEMPLE D'ESTRUCTURA, NO TOCAR (copiar i enganxar per tenir l'estructura feta okey) ///
+public class FlowerGenerator : MonoBehaviour
+{
+    void OnEnable()
+    {
+        GameManager.Instance.Canvi += Reaccio;
+    }
+
+    void OnDisable()
+    {
+        GameManager.Instance.Canvi -= Reaccio;
+    }
+
+    void Reaccio(int estat)
+    {
+        if (estat == GameManager.ESTAT_DISTOPIC)
+        {
+            
+        }
+        else if (estat == GameManager.ESTAT_NORMAL)
+        {
+            
+        }
+        else if (estat == GameManager.ESTAT_UTOPIC)
+        {
+            
+        }
+    }
+}
