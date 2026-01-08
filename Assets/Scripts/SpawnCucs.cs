d2e31477ccc95b183a60a0e123e583f5f2f330e0
emmacasvil <emma.casassas@estudiantat.upc.edu>
2026-01-08 11:02:05 +0100

    cucs

--- Assets/Scripts/SpawnCucs.cs
+++ Assets/Scripts/SpawnCucs.cs
@@ -2,15 +2,58 @@ using UnityEngine;
 
 public class SpawnCucs : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float spawnRateDystopic = 0.5f;   // més ràpid
+    public float spawnRateNormal = 1.5f;     // mig
+    public float spawnRateUtopic = 3f;       // més lent
+
+    private float spawnRateActual;
+    private float timer = 0f;
+
+    void OnEnable()
     {
-        
+        GameManager.Instance.Canvi += Reaccio;
+
+        // Inicialitzar segons l'estat actual
+        Reaccio(GameManager.Instance.estatActual);
     }
 
-    // Update is called once per frame
+    void OnDisable()
+    {
+        GameManager.Instance.Canvi -= Reaccio;
+    }
+
     void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (timer >= spawnRateActual)
+        {
+            SpawnEnemy();
+            timer = 0f;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Debug.Log("[Spawner] Enemy spawned!");
+        // Aquí instancies l’enemic real
+    }
+
+    void Reaccio(int estat)
+    {
+        if (estat == GameManager.ESTAT_DISTOPIC)
+        {
+            spawnRateActual = spawnRateDystopic;
+        }
+        else if (estat == GameManager.ESTAT_NORMAL)
+        {
+            spawnRateActual = spawnRateNormal;
+        }
+        else if (estat == GameManager.ESTAT_UTOPIC)
+        {
+            spawnRateActual = spawnRateUtopic;
+        }
+
+        Debug.Log("[Spawner] Nou spawnRate = " + spawnRateActual);
     }
 }
