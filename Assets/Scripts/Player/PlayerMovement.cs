b007bd19a7256d2c831c16b5f30bceea1e083373
Xeniaa007 <xenia.jordan@estudiantat.upc.edu>
2026-01-08 13:52:44 +0100

    FÍSIQUES ACABADES
    tot explicat

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 using System;
 
+//les físiques del moviment del jugador canviaran de la següent manera:
+//DISTÒPIC: els controls s'invertiran --> dreta serà esquerra, amunt serà avall, etc ...
+//NORMAL: controls normals
+//UTÒPIC: controls normals amb un mini-turbo per anar més ràpid si es prem la tecla d'espai
+
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 3f;
@@ -10,17 +15,17 @@ public class PlayerMovement : MonoBehaviour
     // Moviment base
     [HideInInspector] public Vector2 movement;
 
-    // Estat actual del món
-    private int estatActual = GameManager.ESTAT_NORMAL;
+    //Comencem amb estat distòpic
+    private int estatActual = GameManager.ESTAT_DISTOPIC;
 
     // Turbo
-    public float turboForce = 6f;
-    public float turboDuration = 0.10f;
-    private bool isTurbo = false;
+    public float turboForce = 6f; //la potència del turbo
+    public float turboDuration = 0.10f; //la durada del turbo (és curta perquè no volem un boost molt gran)
+    private bool isTurbo = false; //sempre comencareme en false
     private float turboTimer = 0f;
 
     void Start()
-    {
+    { //llegim els components del player
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
 
@@ -34,6 +39,7 @@ public class PlayerMovement : MonoBehaviour
             GameManager.Instance.Canvi -= OnEstatCanviat;
     }
 
+    //cridem la funció quan el món canvii d'estat
     void OnEstatCanviat(int nouEstat)
     {
         estatActual = nouEstat;
@@ -46,9 +52,10 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         // --- APLICAR FÍSIQUES SEGONS ESTAT ---
+        //fem un switch senzill que controli quan el GameManager canvii d'estat
         switch (estatActual)
         {
-            case GameManager.ESTAT_DISTOPIC:
+            case GameManager.ESTAT_DISTOPIC: //controls invertits
                 moveX = -moveX;
                 moveY = -moveY;
                 break;
@@ -72,34 +79,34 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Speed", movement.magnitude);
     }
 
-    void FixedUpdate()
+    void FixedUpdate() //comprovem que el jugador en aquell frame està fent o no un turbo
     {
         if (isTurbo)
-        {
-            turboTimer -= Time.fixedDeltaTime;
-            if (turboTimer <= 0f)
+        { //el turbo té una durada limitada
+            turboTimer -= Time.fixedDeltaTime; //cada frame li anem restant temps a la duració
+            if (turboTimer <= 0f) //quan el timer arriba a zero el turbo s'acaba
                 isTurbo = false;
 
             return; // mentre dura el turbo, ignorem el moviment normal
         }
 
-        rb.linearVelocity = movement.normalized * speed;
+        rb.linearVelocity = movement.normalized * speed; //un cop acabat el turbo, tornem al moviment normal
     }
 
     // --- TURBO ---
-    void TryTurbo()
+    void TryTurbo() 
     {
         if (movement == Vector2.zero)
-            return; // no turbo si no hi ha direcció
+            return; //si el jugador no es mou en una direcció, no funciona
 
-        StartTurbo(movement.normalized);
+        StartTurbo(movement.normalized); //posem normalized perque funcioni més suau
     }
 
     void StartTurbo(Vector2 direction)
     {
-        isTurbo = true;
+        isTurbo = true; //activem el bool a true (fins ara estava en false)
         turboTimer = turboDuration;
 
-        rb.linearVelocity = direction * turboForce;
+        rb.linearVelocity = direction * turboForce; //la velocitat es multiplicarà amb l'impuls de 6F que hem definit a la variable turboForce
     }
 }
