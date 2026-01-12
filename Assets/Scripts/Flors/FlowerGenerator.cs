242d1c16171a143ff4ec9c4fcad24ecadcf9a059
marinuugh <marina.rodriguez.guasch@estudiantat.upc.edu>
2026-01-12 20:39:20 +0100

    generator

--- Assets/Scripts/Flors/FlowerGenerator.cs
+++ Assets/Scripts/Flors/FlowerGenerator.cs
@@ -1,39 +1,48 @@
-using UnityEngine;                        
-using System.Collections.Generic;          
+using UnityEngine;
+using System.Collections.Generic;
 
-public class FlowerGenerator : MonoBehaviour 
+public class FlowerGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject flowerPrefab; // Prefab de la flor assignable des de l'Inspector.
     [SerializeField] private Transform spawnArea; // Definició del punt d'aparició.
     [SerializeField] private int maxFlors = 15; // Nombre màxim de flors que poden existir alhora.
 
     private List<GameObject> florsActives = new List<GameObject>(); // Llista per guardar referències a les flors creades
-    private int contadorFlors = 3; // Comptador de flors vives/actives a l'inici de la partida. 
+    private int contadorFlors = 0; // Comptador de flors vives/actives a l'inici de la partida. 
 
-    private void OnEnable()                         
+    private void Start()
+    {
+        contadorFlors = 0; //El comptador comença en 0
+        florsActives.Clear();
+        //Generem les dues flors inicials
+        CrearFlor();
+        CrearFlor();
+    }
+
+    private void OnEnable()
     {
         GameManager.Instance.Canvi += Reaccio; // S'inscriu a l'esdeveniment "Canvi" del GameManager per rebre notificacions d'estat
     }
 
-    private void OnDisable()                       
+    private void OnDisable()
     {
         GameManager.Instance.Canvi -= Reaccio; // Es desinscriu de l'esdeveniment per evitar crides quan no toca i fugues de memòria
     }
 
     // Canvi d'estat al game manager
-    void Reaccio(int estat)                         
+    void Reaccio(int estat)
     {
         StopAllCoroutines(); // Atura qualsevol coroutine de generació en marxa per no solapar ritmes de spawn
 
-        if (estat == GameManager.ESTAT_DISTOPIC)    
+        if (estat == GameManager.ESTAT_DISTOPIC)
         {
             StartCoroutine(GenerarFlorsCadaXSegons(45f)); // Comença a generar flors lentament (cada 45s)
         }
-        else if (estat == GameManager.ESTAT_NORMAL) 
+        else if (estat == GameManager.ESTAT_NORMAL)
         {
             StartCoroutine(GenerarFlorsCadaXSegons(30f)); // Generació a ritme mitjà (cada 30s)
         }
-        else if (estat == GameManager.ESTAT_UTOPIC) 
+        else if (estat == GameManager.ESTAT_UTOPIC)
         {
             StartCoroutine(GenerarFlorsCadaXSegons(20f)); // Generació ràpida (cada 20s)
         }
@@ -42,15 +51,15 @@ public class FlowerGenerator : MonoBehaviour
     // Coroutine que crea flors amb un interval
     private System.Collections.IEnumerator GenerarFlorsCadaXSegons(float interval) // Coroutine que s'executa al llarg del temps
     {
-        while (contadorFlors < maxFlors)            
+        while (contadorFlors < maxFlors)
         {
             CrearFlor(); // Crea una nova flor
-            yield return new WaitForSeconds(interval); 
+            yield return new WaitForSeconds(interval);
         }
     }
 
     // Lògica de creació d'una flor
-    private void CrearFlor() 
+    private void CrearFlor()
     {
         // Posició aleatòria dins l’àrea de spawn
         Vector3 posicio = spawnArea.position + new Vector3( // Calcula una posició al voltant del 'spawnArea'
@@ -70,7 +79,7 @@ public class FlowerGenerator : MonoBehaviour
 
     private void OnFlorCanvi(FlowerState flor, int estat) // Es crida quan una flor canvia d'estat
     {
-        if (estat == FlowerState.morta) 
+        if (estat == FlowerState.morta)
         {
             contadorFlors--;
             florsActives.Remove(flor.gameObject); // Treu la referència de la llista d'actives
